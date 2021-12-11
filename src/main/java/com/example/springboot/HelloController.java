426cd03ea049cadc7448cac83384737a7c2566c7
nh4ttruong <truongtbn243@gmail.com>
2021-12-11 13:07:06 -0500

    42nd

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -2,13 +2,17 @@ package com.example.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloController {
 
 	@GetMapping("/")
-	public String index() {
-		return "Greetings from Learnathon Spring Boot! I was try it 30 time :(";
+	public String index(@RequestParam(name="name", required = false) String name, Model model) {
+		model.addAttribute("name", name);
+        if (name == null) return "Hello 54010N";
+		return "Hello " + name;
 	}
 
 }
