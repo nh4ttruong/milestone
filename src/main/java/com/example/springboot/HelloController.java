c74e55e98f057328a5631cc591b590b0eb4690ba
nh4ttruong <truongtbn243@gmail.com>
2021-12-14 06:09:07 -0500

    new source update

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -11,8 +11,7 @@ public class HelloController {
 	@GetMapping("/")
 	public String index(@RequestParam(name="name", required = false) String name, Model model) {
 		model.addAttribute("name", name);
-        if (name == null) return "Hello 54010N";
+        if (name == null) return "Hello 54010N!!!";
 		return "Hello " + name;
 	}
-
 }
